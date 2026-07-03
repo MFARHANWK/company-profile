896f223eaa550b3f872dfab2cc09c281ad291608
Muhammad Farhan Wahyu Kamil <105368993+MFARHANWK@users.noreply.github.com>
2026-07-03 17:56:28 +0700

    feat: add field and ORM model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,12 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasRoles;
 
+    protected $fillable = [
+        'name',
+        'email',
+        'password'
+    ];
+
     /**
      * Get the attributes that should be cast.
      *
