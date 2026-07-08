49b7575f8436be8952873d8a44ad0de9f44e4fea
Muhammad Farhan Wahyu Kamil <105368993+MFARHANWK@users.noreply.github.com>
2026-07-08 22:26:42 +0700

    feat: connect CMS to blade

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyStatistic;
+use App\Models\HeroSection;
+use App\Models\OurPrinciple;
+use App\Models\OurTeam;
+use App\Models\Product;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     //
-    public function index(){
-        return view('front.index');
+    public function index()
+    {
+        $principles = OurPrinciple::take(4)->get();
+        $statistics = CompanyStatistic::take(4)->get();
+        $products = Product::take(4)->get();
+        $teams = OurTeam::take(7)->get();
+        $testimonials = Testimonial::take(7)->get();
+        $hero_section = HeroSection::OrderByDesc('id')->take(1)->get();
+        return view('front.index', compact('statistics', 'principles', 'products', 'teams', 'testimonials', 'hero_section'));
     }
 }
