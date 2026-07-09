cf26928d1890f9c709c6e0a9ac7628ea58bba331
Muhammad Farhan Wahyu Kamil <105368993+MFARHANWK@users.noreply.github.com>
2026-07-09 17:43:57 +0700

    feat: add appointment page

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreAppointmentRequest;
+use App\Models\Appointment;
 use App\Models\CompanyAbout;
 use App\Models\CompanyStatistic;
 use App\Models\HeroSection;
@@ -10,6 +12,7 @@ use App\Models\OurTeam;
 use App\Models\Product;
 use App\Models\Testimonial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class FrontController extends Controller
 {
@@ -37,5 +40,19 @@ class FrontController extends Controller
         $statistics = CompanyStatistic::take(4)->get();
         $abouts = CompanyAbout::take(2)->get();
         return view('Front.about', compact('statistics', 'abouts'));
+    }
+
+    public function appointment(){
+        $testimonials = Testimonial::take(7)->get();
+        $products = Product::take(3)->get();
+        return view('Front.appointment', compact('testimonials', 'products'));
+    }
+
+    public function appointment_store(StoreAppointmentRequest $request){
+        DB::transaction(function () use($request){
+            $validated = $request->validated();
+            $newAppointment = Appointment::create($validated);
+        });
+        return redirect()->route('front.index');
     }
 }
