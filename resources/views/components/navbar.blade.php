ad7acf965bcac226f3c8f6066c5bc04aa43a7845
Muhammad Farhan Wahyu Kamil <105368993+MFARHANWK@users.noreply.github.com>
2026-07-09 21:16:13 +0700

    fix: fix navbar and routing

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -24,7 +24,7 @@
         <li class="{{ request()->routeIs('front.about') ? 'text-cp-dark-blue' : '' }} font-semibold hover:text-cp-dark-blue transition-all duration-300">
             <a href="{{ route('front.about') }}">About</a>
     </ul>
-    <a href=""
+    <a href="{{ route('front.appointment') }}"
         class="bg-cp-dark-blue p-[14px_20px] w-fit rounded-xl hover:shadow-[0_12px_30px_0_#312ECB66] transition-all duration-300 font-bold text-white">Get
         a Quote</a>
 </nav>
